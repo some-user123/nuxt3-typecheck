1a2c02d864bfabf8816aaf358029602c68138304
some-user123 <74715856+some-user123@users.noreply.github.com>
2022-08-23 16:24:50 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+Hello World <br />
+{{ article }}
+</div>
+</template>
+
+<script setup lang="ts">
+interface Article {
+  id: number
+}
+const article = await $fetch<Article>('https://jsonplaceholder.typicode.com/todos/1')
+</script>
